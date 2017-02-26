ad14127f06868e38d0ef5f86587a4a6d750de119
Alden Ling <aldenling@Aldens-MacBook-Pro.local>
2017-02-26 00:02:26 -0800

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -30,7 +31,22 @@ app.get('/todos', (req, res) => {
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+   var id = req.params.id;
 
+   if (!ObjectID.isValid(id)){
+       return res.status(400).send({});
+   }
+
+   Todo.findById(id).then((todos) => {
+       if (!todos) {
+           return res.status(400).send({});
+       }
+       res.send({todos});
+   }).catch((e) => {
+       res.status(400).send({});
+   })
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
